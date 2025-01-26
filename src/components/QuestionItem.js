4ec06ed13471fd935f82897dd900b9bd434cef74
Kanana-Mwenda <Lizamwenda95@gmail.com>
2025-01-26 20:31:54 +0300

    commit message

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion,onUpdateQuestion}) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,33 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleCorrectAnswerChange(){
+    fetch(`http://localhost:4000/questions/${question.id}`,{
+      method:"PATCH",
+      headers:{ "Content-Type": "application/json"},
+      body:JSON.stringify({"correctIndex": correctIndex }),
+    })
+    .then(response => response.json())
+    .then(updatedQuestion => onUpdateQuestion(updatedQuestion))
+  }
+  function handleDeleteQuestionClick(){
+    fetch(`http://localhost:4000/questions/${question.id}`,{
+      method:"DELETE",
+    })
+    .then(response => response.json())
+    .then(() => onDeleteQuestion(question));
+  }
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex}>{options}
+          onChange={handleCorrectAnswerChange}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteQuestionClick}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
+import QuestionItem from "./QuestionItem";
+import QuestionForm from "./QuestionForm";
 
 function QuestionList() {
+const[question,setQuestion]= useState([]);
+
+useEffect(() => {
+  fetch("http://localhost:4000/questions")
+  .then(response => response.json())
+  .then(data => setQuestion(data));
+}, []);
+
+function handleAddQuestion (newQuestion){
+  setQuestion([...question,newQuestion]);
+}
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <QuestionForm onAddQuestion ={handleAddQuestion}/>
+      <ul>
+        {question.map((question) => (
+     <QuestionItem  key={question.id} question={question}/>))}</ul>
     </section>
   );
 }
